5355b07e2eb393cb2e73ca36938d0ad8ed37bd66
denuwan55 <ldweerarathne5555@gmail.com>
2022-06-30 00:19:50 +0530

    database setup

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 const express = require('express')
-// const colors = require('colors')
+const colors = require('colors')
 const cors = require('cors')
 const dotenv = require('dotenv').config()
 // const {errorHandler} = require('./middleware/errorMiddleware')
-// const connectDB = require('./config/db')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
 const app = express();
 
-// connectDB();
+connectDB();
 
 app.use(express.json())
 app.use(cors())
@@ -16,14 +16,14 @@ app.use(express.urlencoded({extended: false}))
 
 /* ROUTES */
 
-// image routes
-app.use('/api/images', require('./routes/imageRoutes.js'))
-// consultant routes
-app.use('/api/consultants', require('./routes/consultantRoutes.js'))
-// doctor routes
-app.use('/api/doctors', require('./routes/doctorRoutes.js'))
-// admin routes
-app.use('/api/admin', require('./routes/adminRoutes.js'))
+// // image routes
+// app.use('/api/images', require('./routes/imageRoutes.js'))
+// // consultant routes
+// app.use('/api/consultants', require('./routes/consultantRoutes.js'))
+// // doctor routes
+// app.use('/api/doctors', require('./routes/doctorRoutes.js'))
+// // admin routes
+// app.use('/api/admin', require('./routes/adminRoutes.js'))
 
 // app.use(errorHandler)
 
